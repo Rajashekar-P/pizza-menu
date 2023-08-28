318886b9eb97e02e9d1476f53dbc072473922045
Rajashekar Pallala <shekhar.pallala@gmail.com>
2023-08-28 16:35:27 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,103 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
+import "./index.css";
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  }
+]
+
+function App(){
+  return <div className="container">
+    <Header />
+    <Menu />
+    <Footer />
+    </div>
+}
+
+function Header() {
+  // const style = { color: "red", fontSize: '32px', textTransform: "uppercase" };
+  const style = {};
+
+  return (
+    <header className="header">
+      <h1 style={style}>
+        Fast React Pizza Co.
+      </h1>
+    </header>
+  );
+}
+
+function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
+
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+
+      {numPizzas > 0 ? (
+      <ul className="pizzas">
+        {pizzas.map((pizza) => (
+          <Pizza pizzaObj={pizza} key={pizza.name}/>
+        ))}
+      </ul>
+      ): null}
+    </main>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour
+
+
+  return (
+  <footer className="footer">
+    {isOpen ? (
+      <div className="order">
+        <p>We're open until {closeHour}:00. Come visit us or Order Online</p>
+        <button className="btn">Order</button>
+      </div>
+    ): (
+      <p>We're happy to welcome you between {openHour}:00 and {closeHour}:00.</p>
+    )}
+  </footer>
+  );
+}
+
+function Pizza(props){
+  return(
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <div>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price}</span>
+      </div>
+    </li>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// comment added
